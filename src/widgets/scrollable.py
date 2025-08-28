d161bad75ab595b677dd222c55c953546a0075a3
SaadZahem <saadzahem7@gmail.com>
2025-08-28 09:53:46 +0300

    fix: reduced redundant updates when resizing the scrollable window

--- src/widgets/scrollable.py
+++ src/widgets/scrollable.py
@@ -4,7 +4,60 @@ from tkinter import ttk
 from typing import Literal, Callable, Any
 
 
+def debounce(wait: int):
+    """
+    Tkinter-safe debounce decorator.
+    Auto-detects root using the widget from the first call.
 
+    Args:
+        widget (tk.Widget): Any widget (usually root) to call `after()` on.
+        wait (int): Delay in milliseconds before executing the function.
+    """
+
+    def decorator(fn: Callable):
+        job = None
+        top = None
+
+        @functools.wraps(fn)
+        def debounced(*args: Any, **kwargs: Any):
+            nonlocal top, job
+
+            # Parse arguments
+            while top is None:
+                if not args:
+                    error = "No arguments were given"
+                    break
+
+                first = args[0]
+
+                # Infer widget from first argument if it is a widget or an event
+                if isinstance(first, tk.Widget):
+                    top = first.winfo_toplevel()
+                elif isinstance(first, tk.Event):
+                    top = first.widget.winfo_toplevel()
+                else:
+                    error = f"First argument {first!r}."
+
+                break
+
+            # Report any error
+            if top is None:
+                errmsg = "Couldn't infer Tkinter root widget. "
+                try:
+                    raise ValueError(errmsg + error)  # type: ignore
+                except NameError:
+                    raise ValueError(errmsg) from None
+
+            # Cancel previous job
+            if job is not None:
+                top.after_cancel(job)
+            
+            # Create a new one
+            job = top.after(wait, lambda: fn(*args, **kwargs))
+
+        return debounced
+
+    return decorator
 
 
 class Scrollable(tk.Canvas):
@@ -24,7 +77,7 @@ class Scrollable(tk.Canvas):
         self.scrollbar = ttk.Scrollbar(parent, orient=orient, command=self.view)
         self.configure({self.select(*"yx") + "scrollcommand": self.scrollbar.set})
 
-        # Bind resizing events. Might result in redundant updates!!
+        # Bind resizing events.
         self.frame.bind("<Configure>", self._update_scrollregion)
         self.bind("<Configure>", self._resize_canvas)
 
@@ -43,12 +96,12 @@ class Scrollable(tk.Canvas):
     def view(self):
         return self.xview if self.orient == "horizontal" else self.yview
 
-
+    @debounce(1000)
     def _update_scrollregion(self, event=None):
         self.configure(scrollregion=self.bbox("all"))
         self._check_scrollbar()
 
-
+    @debounce(50)
     def _resize_canvas(self, event=None):
         dim = self.select("width", "height")
         self.itemconfig(self._window, {dim: getattr(self, f"winfo_{dim}")()})
