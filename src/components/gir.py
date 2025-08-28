aa3a9fd45e66ba56d2af5d558da0075a489e464e
SaadZahem <saadzahem7@gmail.com>
2025-08-28 09:51:36 +0300

    feat: basic UI (gir-calc)

---
+++ src/components/gir.py
@@ -0,0 +1,173 @@
+from datetime import datetime, date
+from tkcalendar import DateEntry
+import tkinter as tk
+from tkinter import ttk
+from typing import NamedTuple, Callable
+
+
+class InputItem[T](NamedTuple):
+    validate: Callable = str
+    factory: Callable = str
+    choices: list[T] | None = None
+
+
+class GIRFrame(ttk.Frame):
+    CROP_TYPES = [
+        "Wheat",
+        "Rice",
+    ]
+
+    def __init__(self, master: tk.Misc):
+        super().__init__(master)
+
+        self.columnconfigure(2, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        # links
+        self.dates: list[DateEntry] = []
+
+        # styles
+        from .styles import bold_font
+
+        # head
+        head = ttk.Frame(self)
+        self.head_details = {
+            "Crop": InputItem[str](choices=self.CROP_TYPES),
+            "Plant Date": InputItem(factory=datetime),
+            "Harvest Date": InputItem(factory=datetime),
+            "errmsg": ttk.Label(
+                head, anchor="center", foreground="red", font=bold_font
+            ),
+            "sep": ttk.Separator(head, orient="horizontal"),
+            "Area (m²)": InputItem(float),
+            "AW": InputItem(float),
+            "Ece": InputItem(float),
+            "Ecw": InputItem(float),
+            "MAP": InputItem(float),
+            "Zn": InputItem(float),
+            "Zo": InputItem(float),
+            "Zx": InputItem(float),
+            "to": InputItem(float),
+            "tx": InputItem(float),
+            "h": InputItem(float),
+        }
+        for row, (label, item) in enumerate(self.head_details.items()):
+            if isinstance(item, ttk.Widget):
+                item.grid(row=row, column=0, columnspan=3, sticky="WE", pady=8)
+                continue
+
+            ttk.Label(head, text=label, font=bold_font).grid(column=0, row=row)
+
+            if item.choices:
+                widget = ttk.Combobox(head, values=item.choices)
+
+            elif item.factory is datetime:
+                # current issue: calendar dropdown is self-destroyed when the widget is originally focused
+                widget = DateEntry(head, date_pattern="y-mm-dd")
+                widget.bind("<<DateEntrySelected>>", self.verify_dates)
+                self.dates.append(widget)
+            else:
+                widget = ttk.Entry(head, justify="center")
+
+            widget.grid(row=row, column=1, sticky="W", columnspan=2)
+
+        head.grid(row=0, column=0, sticky="NWS")
+
+        # separator
+        ttk.Separator(self, orient="vertical").grid(
+            row=0, column=1, padx=8, sticky="ns"
+        )
+
+        # body
+        self.canvas = canvas = tk.Canvas(self, borderwidth=0)
+        scrollbar = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
+        body = ttk.Frame(canvas)
+        
+        canvas.grid(row=0, column=2, sticky="NEWS")
+        scrollbar.grid(row=0, column=3, sticky="NS")
+        
+        canvas.create_window((0, 0), window=body, anchor="nw", tags="self.frame")
+        canvas.configure(yscrollcommand=scrollbar.set)
+        canvas.bind("<Configure>", lambda e: canvas.itemconfig("self.frame", width=e.width))
+        
+        for event in {"MouseWheel", "Button-4", "Button-5"}:
+            canvas.bind_all(f"<{event}>", self._on_mousewheel)
+        
+        body.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all"),
+                width=body.winfo_reqwidth(),
+            )
+        )
+        
+        inputs = {
+            "Tmin (C)": "tmin",
+            "Tmax (C)": "tmax",
+            "Humidity (%)": "humidity",
+            "Wind Speed (m/s)": "wind_speed",
+            "Sun (hour)": "sun_hours",
+        }
+        outputs = {"ETr (mm/day)": ""}
+        loop = {
+            "": lambda r: date(date.today().year, r, 1).strftime("%B"),
+            **inputs,
+            **outputs,
+        }
+        for row in range(61):
+            for column, (key, value) in enumerate(loop.items()):
+                if row == 0:
+                    widget = ttk.Label(body, text=key, font=bold_font)
+
+                elif key in inputs:
+                    variable = tk.StringVar()
+                    widget = ttk.Entry(
+                        body,
+                        justify="center",
+                        validate="focusout",
+                        # validatecommand=(self.register(self.validate), "%P", "%W"),
+                        textvariable=variable,
+                    )
+                    # self.variables[value, row] = variable
+                    # self.widgets[str(widget)] = row
+
+                elif column == 0:
+                    widget = ttk.Label(
+                        body, text=row, style="Const.TLabel", anchor="center"
+                    )
+                else:
+                    variable = tk.StringVar()
+                    widget = ttk.Label(
+                        body,
+                        text=value,
+                        style="Output.TLabel",
+                        textvariable=variable,
+                        anchor="center",
+                    )
+                    # self.outcome[row] = variable
+
+                widget.grid(row=row, column=column, sticky="NEWS", ipadx=5)
+
+    def verify_dates(self, event):
+        plant, harvest = self.dates
+
+        if plant.get_date() < harvest.get_date():
+            msg = ""
+        else:
+            msg = "Error:\nharvest date must be after plant date"
+
+        self.head_details["errmsg"].configure(text=msg)
+    
+    def _on_mousewheel(self, event):
+        if event.num == 4:   # Linux scroll up
+            self.canvas.yview_scroll(-1, "units")
+        elif event.num == 5: # Linux scroll down
+            self.canvas.yview_scroll(1, "units")
+        else:  # Windows / macOS
+            self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    GIRFrame(root).pack(fill="both", expand=1)
+    root.mainloop()
