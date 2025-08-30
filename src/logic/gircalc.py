4de7831139954031541b73f08fc0f0ddf16dc02d
SaadZahem <saadzahem7@gmail.com>
2025-08-30 03:08:48 +0300

    refactor: replaced functions with a class

--- src/logic/gircalc.py
+++ src/logic/gircalc.py
@@ -14,32 +14,63 @@ dg = IF(lr > 0.1, di / 0.55 * (1 - lr), di / 0.55)
 gir = dg * a * 4200 / 1000
 """
 
-
-def find_zr(doy: int, to: int, tx: int, zo: float, zx: float, n: float) -> float:
-    if doy < to:
-        return zo
-
-    return zo + (zx - zo) * ((doy - to / 2) / (tx - to / 2)) ** (1 / n)
+from typing import NamedTuple
 
 
-def find_taw_raw(aw: float, zr: float, mad: float) -> tuple[float, float]:
-    taw = aw * zr
-    raw = taw * mad
-    return taw, raw
+class Params(NamedTuple):
+    """The interface between the UI and the functions. Carries all params required by all functions."""
 
+    # global
+    area: float
+    aw: float
+    ece: float
+    ecw: float
+    mad: float
+    zn: float
+    zo: float
+    zx: float
+    to: int
+    tx: int
+    n: float
 
-def find_gir(a, ece, ecw, eta, p):
-    di = eta - p
-    lr = ecw / (5 * ece - ecw)
+    # local
+    doy: int
+    eta: float
+    precipitation: float
 
-    if lr > 0.1:
-        dg = di / 0.55 * (1 - lr)
-    else:
-        dg = di / 0.55
+    @property
+    def zr(self) -> float:
+        if self.doy < self.to:
+            return self.zo
 
-    gir = dg * a * 4200 / 1000
+        return self.zo + (self.zx - self.zo) * (
+            (self.doy - self.to / 2) / (self.tx - self.to / 2)
+        ) ** (1 / self.n)
 
-    return gir
+    @property
+    def taw(self) -> float:
+        return self.aw * self.zr
+
+    @property
+    def raw(self) -> float:
+        return self.taw * self.mad
+
+    @property
+    def di(self) -> float:
+        return self.eta - self.precipitation
+
+    @property
+    def lr(self) -> float:
+        return self.ecw / (5 * self.ece - self.ecw)
+
+    @property
+    def gir(self) -> float:
+        di = self.di
+        if self.lr > 0.1:
+            dg = di / 0.55 * (1 - self.lr)
+        else:
+            dg = di / 0.55
+        return dg * self.area * 4200 / 1000
 
 
 if __name__ == "__main__":
